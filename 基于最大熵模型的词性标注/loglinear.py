6c6f4c7ec3e3f1f93ccb3e0a2af786b31d9fabd2
2309063688 <55655779+2309063688@users.noreply.github.com>
2022-03-02 13:32:20 +0800

    Add files via upload

---
+++ 基于最大熵模型的词性标注/loglinear.py
@@ -0,0 +1,172 @@
+import pickle
+import random
+from collections import defaultdict
+from datetime import datetime, timedelta
+
+import numpy as np
+from scipy.special import logsumexp
+
+class LogLinearModel(object):
+    def __init__(self, nt):
+        self.nt = nt
+
+    def create_fspace(self, data):
+        # 特征空间
+        self.epsilon = list({
+            f for wiseq, tiseq in data
+            for i, ti in enumerate(tiseq)
+            for f in self.instantialize(wiseq, i, ti)
+        })
+        # 特征对应索引的字典
+        self.fdict = {f: i for i, f in enumerate(self.epsilon)}
+        # 特征空间维度
+        self.d = len(self.epsilon)
+
+        # 特征权重
+        self.W = np.zeros(self.d)
+
+    def predict(self, wiseq, index):
+        fvs = [self.instantialize(wiseq, index, ti)
+               for ti in range(self.nt)]
+        scores = np.array([self.f(fv) for fv in fvs])
+        return np.argmax(scores)
+
+    def f(self, fv):
+        scores = [self.W[self.fdict[f]]
+                  for f in fv if f in self.fdict]
+        return sum(scores)
+
+    # 特征模板
+    def instantialize(self, wiseq, idx, ti):
+        word = wiseq[idx]
+        prev_word = wiseq[idx - 1] if idx > 0 else '^^'
+        next_word = wiseq[idx + 1] if idx < len(wiseq) - 1 else '$$'
+        prev_char = prev_word[-1]
+        next_char = next_word[0]
+        first_char = word[0]
+        last_char = word[-1]
+
+        fv = []
+        fv.append(('02', ti, word))
+        fv.append(('03', ti, prev_word))
+        fv.append(('04', ti, next_word))
+        fv.append(('05', ti, word, prev_char))
+        fv.append(('06', ti, word, next_char))
+        fv.append(('07', ti, first_char))
+        fv.append(('08', ti, last_char))
+
+        for char in word[1:-1]:
+            fv.append(('09', ti, char))
+            fv.append(('10', ti, first_char, char))
+            fv.append(('11', ti, last_char, char))
+
+        if len(word) == 1:
+            fv.append(('12', ti, word, prev_char, next_char))
+
+        for i in range(1, len(word)):
+            prechar, char = word[i-1], word[i]
+            if prev_char == char:
+                fv.append(('13', ti, char, 'consecutive'))
+
+        if len(word) <= 4:
+            fv.append(('14', ti, word))
+            fv.append(('15', ti, word))
+        else:
+            for i in range(1, 5):
+                fv.append(('14', ti, word[: i]))
+                fv.append(('15', ti, word[-i:]))
+
+        return fv
+
+    def SGD(self, trainset, devset, file, epochs, batch_size,
+            interval, eta, decay, lmbda):
+        total_time = timedelta()
+
+        max_epoch, max_acc = 0, 0.0
+        #为退火统计更新次数
+        count = 0
+
+        traindata = [(wiseq, i, ti) for wiseq, tiseq in trainset
+                     for i, ti in enumerate(tiseq)]
+        #训练集大小
+        n = len(traindata)
+
+        for epoch in range(1, epochs + 1):
+            start = datetime.now()
+            #打乱数据
+            random.shuffle(traindata)
+            #分批
+            batches = [traindata[i:i+batch_size] for i in range(0,n,batch_size)]
+            nb = len(batches)
+
+            for batch in batches:
+                self.update(batch,lmbda, n, eta*decay**(count/nb))
+                count+=1
+
+            # 输出一次epoch的信息
+            print("Epoch:" + str(epoch) + "/" + str(epochs))
+            acc, total, accuracy = self.evaluate(trainset)
+            print("train:" + str(acc) + "/" + str(total) + " = " + str(accuracy))
+            acc, total, accuracy = self.evaluate(devset)
+            print("dev:" + str(acc) + "/" + str(total) + " = " + str(accuracy))
+            t = datetime.now() - start
+            print("time cost:" + str(t))
+            total_time += t
+
+            # 保存效果最好的模型
+            if accuracy > max_acc:
+                self.dump(file)
+                max_epoch, max_acc = epoch, accuracy
+            elif epoch - max_epoch > interval:
+                break
+
+        print("max accuracy:" + str(max_acc))
+        print("epoch:" + str(max_epoch))
+        print("mean time:" + str(total_time / epoch))
+
+    def update(self, batch, lmbda, n, eta):
+        gradients = defaultdict(float)
+
+        for wiseq, i, ti in batch:
+            fv = self.instantialize(wiseq, i, ti)
+            fiseq = (self.fdict[f] for f in fv if f in self.fdict)
+            #梯度向量
+            for fi in fiseq:
+                gradients[fi] += 1
+
+            #获取每个词性对应的所有特征
+            fvs = [self.instantialize(wiseq, i, ti) for ti in range(self.nt)]
+            scores = np.array([self.f(fv) for fv in fvs])
+            probs = np.exp(scores - logsumexp(scores))
+            #更新梯度向量
+            for fv, p in zip(fvs, probs):
+                fiseq = (self.fdict[f] for f in fv if f in self.fdict)
+                for fi in fiseq:
+                    gradients[fi] -= p
+
+            #更新权重
+            self.W *= (1 - eta * lmbda / n)
+            for key,value in gradients.items():
+                self.W[key] += eta * value
+
+    # 评价函数
+    def evaluate(self, data):
+        acc, total = 0, 0
+
+        for wiseq, tiseq in data:
+            total += len(wiseq)
+            p_tiseq = np.array([self.predict(wiseq, i)
+                                for i in range(len(wiseq))])
+            acc += np.sum(tiseq == p_tiseq)
+        accuracy = acc / total
+        return acc, total, accuracy
+
+    def dump(self, file):
+        with open(file, 'wb') as f:
+            pickle.dump(self, f)
+
+    @classmethod
+    def load(cls, file):
+        with open(file, 'rb') as f:
+            llm = pickle.load(f)
+        return llm
--- 基于最大熵模型的词性标注/run.py
+++ 基于最大熵模型的词性标注/run.py
@@ -1,4 +1,47 @@
+from corpus import Corpus
+from loglinear import LogLinearModel
+import random
+from datetime import datetime, timedelta
+
+TRAIN = 'data/train.conll'
+DEV = 'data/dev.conll'
+file = 'data/model.config'
+
 epochs = 100
 batch_size = 50
 lmbda = 0.01
+interval = 5
+eta = 0.5
+decay = 0.96
 
+print("Preprocess:")
+corpus = Corpus(TRAIN)
+print(corpus)
+
+print("Load dataset:")
+trainset = corpus.load(TRAIN)
+devset = corpus.load(DEV)
+print("     size of trainset:" + str(len(trainset)))
+print("     size of devset:" + str(len(devset)))
+
+start = datetime.now()
+
+print("Create loglinear model")
+llm = LogLinearModel(corpus.nt)
+
+llm.create_fspace(trainset)
+
+
+
+print("Use SGD to train")
+print("  epochs:" + str(epochs))
+print("  batch_size:" + str(batch_size))
+print("  interval:" + str(interval))
+print("  eta:" + str(eta))
+print("  decay:" + str(decay))
+print("  lmbda:" + str(lmbda))
+print()
+llm.SGD(trainset=trainset, devset=devset,file=file, epochs=epochs,
+        batch_size=batch_size, interval=interval, eta=eta, decay=decay, lmbda=lmbda)
+
+print("time cost:" + str(datetime.now() - start))
